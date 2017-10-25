638a93d787ddf23a2976b92feb8bb7530eac8755
Changle Xie <xcl5314@gmail.com>
2017-10-25 16:29:55 +0800

    upload

---
+++ consts.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+HOSTNAME = 'localhost'
+DATABASE = 'r'
+USERNAME = 'root'
+PASSWORD = 'xcl531493190'
+DB_URI = 'mysql://{}:{}@{}/{}'.format(USERNAME, PASSWORD, HOSTNAME, DATABASE)
